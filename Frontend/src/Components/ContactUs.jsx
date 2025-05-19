248f481f1230cd0475762d7e0963783d64906131
NikhiDevadiga <139534147+NikhiDevadiga@users.noreply.github.com>
2025-05-19 14:39:22 +0530

    Update ContactUs.jsx

--- Frontend/src/Components/ContactUs.jsx
+++ Frontend/src/Components/ContactUs.jsx
@@ -34,7 +34,7 @@ const ContactUs = () => {
     setSubmitSuccess(false);
   
     try {
-      const response = await fetch("http://localhost:5000/api/contact", {
+      const response = await fetch("https://manglore-store-t98r.onrender.com/api/contact", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
