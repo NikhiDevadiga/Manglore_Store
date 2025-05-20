8bd067c5ca4542ee903157394520fa91ca82f79f
NikhiDevadiga <139534147+NikhiDevadiga@users.noreply.github.com>
2025-05-20 14:28:00 +0530

    Update Navbar.jsx

--- Frontend/src/Components/Navbar.jsx
+++ Frontend/src/Components/Navbar.jsx
@@ -12,10 +12,13 @@ import axios from 'axios';
 import './Navbar.css';
 import CartDrawer from './CartDrawer';
 import ProfileDrawer from './ProfileDrawer';
+import { useWishlist } from '../context/Wishlist'
 
 const Navbar = () => {
   const { cartItems = [] } = useCart();
   const totalItems = cartItems.reduce((total, item) => total + item.quantity, 0);
+  const { wishlist } = useWishlist();
+  const wishlistCount = wishlist.length;
   const navigate = useNavigate();
   const isTablet = useMediaQuery('(max-width:768px)');
   const isMobile = useMediaQuery('(max-width:550px)');
@@ -41,7 +44,7 @@ const Navbar = () => {
     const fetchOrders = async () => {
       setLoadingOrders(true);
       try {
-        const res = await axios.get(`https://manglore-store-t98r.onrender.com/api/user/${user._id}`);
+        const res = await axios.get(`/api/user/${user._id}`);
         setOrders(res.data);
       } catch (err) {
         console.error("Failed to fetch orders:", err);
@@ -75,7 +78,7 @@ const Navbar = () => {
   useEffect(() => {
     const handler = setTimeout(() => {
       if (searchValue.trim()) {
-        axios.get(`https://manglore-store-t98r.onrender.com/api/product?query=${searchValue}`)
+        axios.get(`http://localhost:5000/api/product?query=${searchValue}`)
           .then(res => setSearchResults(res.data.data))
           .catch(err => console.error("Search API error:", err));
       } else {
@@ -96,7 +99,7 @@ const Navbar = () => {
   const handleProfileUpdate = async () => {
     try {
       setUpdatingProfile(true);
-      await axios.put(`https://manglore-store-t98r.onrender.com/api/update/${user._id}`, editProfileData);
+      await axios.put(`http://localhost:5000/api/update/${user._id}`, editProfileData);
       localStorage.setItem('user', JSON.stringify({ ...user, ...editProfileData }));
       setUserDetails(prev => ({ ...prev, ...editProfileData }));
     } catch (error) {
@@ -149,7 +152,7 @@ const Navbar = () => {
         {!isTablet && (
           <>
             <Tooltip title={location} arrow>
-              <Button startIcon={<MdMyLocation />} variant="outlined" sx={{color:"#9d66bd", borderColor:"#9d66bd"}} onClick={() => setOpenDialog(true)} className="location-btn">
+              <Button startIcon={<MdMyLocation />} variant="outlined" sx={{ color: "#9d66bd", borderColor: "#9d66bd" }} onClick={() => setOpenDialog(true)} className="location-btn">
                 <Box className="location-text">{location}</Box>
               </Button>
             </Tooltip>
@@ -169,7 +172,15 @@ const Navbar = () => {
             </Box>
 
             <Box className="navbar-icons">
-              <div onClick={() => handleIconClick('wishlist')} aria-label="wishlist"><FaHeart size={20} /></div>
+              <div onClick={() => handleIconClick('wishlist')} aria-label="wishlist">
+                <Badge
+                  variant="dot"
+                  color="secondary"
+                  invisible={wishlistCount === 0}
+                >
+                  <FaHeart size={20} />
+                </Badge>
+              </div>
               {user && (
                 <div onClick={() => handleIconClick('cart')} aria-label="cart">
                   <Badge badgeContent={totalItems > 0 ? totalItems : null} color="secondary">
@@ -199,7 +210,7 @@ const Navbar = () => {
       <Dialog open={openDialog} onClose={() => setOpenDialog(false)}>
         <DialogTitle>Select a location for delivery</DialogTitle>
         <DialogContent>
-          <Button onClick={handleDetectLocation} fullWidth variant="contained" sx={{backgroundColor: "#02002ee0",color: "#fff"}} startIcon={<MdMyLocation />}>
+          <Button onClick={handleDetectLocation} fullWidth variant="contained" sx={{ backgroundColor: "#02002ee0", color: "#fff" }} startIcon={<MdMyLocation />}>
             Detect My Location
           </Button>
         </DialogContent>
@@ -224,29 +235,29 @@ const Navbar = () => {
       <CartDrawer open={openCartDrawer} onClose={() => setOpenCartDrawer(false)} />
 
       <Drawer anchor="left" open={openMobileDrawer} onClose={() => setOpenMobileDrawer(false)}>
-         <Box role="presentation" sx={{ width: 260, p: 2, height: '100%', display: 'flex', flexDirection: 'column' }}>
-           <Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
-             <Typography variant="h6" fontWeight="bold">Menu</Typography>
-             <IconButton onClick={() => setOpenMobileDrawer(false)}>
+        <Box role="presentation" sx={{ width: 260, p: 2, height: '100%', display: 'flex', flexDirection: 'column' }}>
+          <Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+            <Typography variant="h6" fontWeight="bold">Menu</Typography>
+            <IconButton onClick={() => setOpenMobileDrawer(false)}>
               <span style={{ fontSize: 12 }}>×</span>
-             </IconButton>
-           </Box>
-           <Box sx={{ display: 'flex', flexDirection: 'column', mt: 2 }}>
-             <Button onClick={() => handleIconClick('wishlist')} variant="text" sx={{ justifyContent: 'flex-start', color:'#02002ee0',  }}>
+            </IconButton>
+          </Box>
+          <Box sx={{ display: 'flex', flexDirection: 'column', mt: 2 }}>
+            <Button onClick={() => handleIconClick('wishlist')} variant="text" sx={{ justifyContent: 'flex-start', color: '#02002ee0', }}>
               Wishlist
             </Button>
-            <Button onClick={() => handleIconClick('cart')} variant="text" sx={{ justifyContent: 'flex-start', color:'#02002ee0' }}>
-               Cart
-             </Button>
-             <Button onClick={() => handleIconClick('profile')} variant="text" sx={{ justifyContent: 'flex-start', color:'#02002ee0' }}>
-               User Profile
-             </Button>
-             <Button onClick={() => setOpenDialog(true)} variant="text" sx={{ justifyContent: 'flex-start', color:'#02002ee0'}}>
-               your Location
-             </Button>
-           </Box>
-         </Box>
-       </Drawer>
+            <Button onClick={() => handleIconClick('cart')} variant="text" sx={{ justifyContent: 'flex-start', color: '#02002ee0' }}>
+              Cart
+            </Button>
+            <Button onClick={() => handleIconClick('profile')} variant="text" sx={{ justifyContent: 'flex-start', color: '#02002ee0' }}>
+              User Profile
+            </Button>
+            <Button onClick={() => setOpenDialog(true)} variant="text" sx={{ justifyContent: 'flex-start', color: '#02002ee0' }}>
+              your Location
+            </Button>
+          </Box>
+        </Box>
+      </Drawer>
     </>
   );
 };
